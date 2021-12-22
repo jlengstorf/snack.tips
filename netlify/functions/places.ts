f880c3417acd8ede24b306afcf208b3ed7525861
Jason Lengstorf <jason@lengstorf.com>
2021-12-21 23:00:14 -0800

    feat: add types, sorting, filtering

--- netlify/functions/places.ts
+++ netlify/functions/places.ts
@@ -1,7 +1,36 @@
 import { Handler, builder } from '@netlify/functions';
 import fetch from 'node-fetch';
 
-export const handler: Handler = builder(async () => {
+type SanityPlaceResponse = {
+  name: string;
+  slug: {
+    current: string;
+  };
+  note: string;
+  url: string;
+  address: string;
+  location: {
+    lng: number;
+    lat: number;
+  };
+  city: {
+    slug: {
+      current: string;
+    };
+  };
+};
+
+type FetchResult = {
+  data?: {
+    allPlace: SanityPlaceResponse[];
+  };
+  errors?: Array<{ message: string }>;
+};
+
+export const handler: Handler = builder(async (event) => {
+  const params = event.queryStringParameters;
+  const city = params?.city || 'portland';
+
   const response = await fetch(
     'https://3bbpgel8.api.sanity.io/v1/graphql/production/default',
     {
@@ -11,8 +40,11 @@ export const handler: Handler = builder(async () => {
       },
       body: JSON.stringify({
         query: `
-          query LoadAllPlaces {
-            allPlace {
+          query LoadAllPlaces($city: String!) {
+            allPlace(
+              where: { city: { slug: { current: { eq: $city } } } },
+              sort: { name: ASC }
+            ) {
               _id
               name
               slug {
@@ -34,12 +66,14 @@ export const handler: Handler = builder(async () => {
             }
           }
         `,
-        variables: {},
+        variables: { city },
       }),
     },
   );
 
-  if (!response.ok) {
+  const result = (await response.json()) as FetchResult;
+
+  if (!response.ok || !result.data) {
     console.error(response);
     return {
       statusCode: 500,
@@ -47,24 +81,24 @@ export const handler: Handler = builder(async () => {
     };
   }
 
-  const { data } = await response.json();
-
-  const features = data.allPlace.map((place, index: number) => ({
-    type: 'Feature',
-    id: index + 1, // Mapbox requires a numerical ID
-    geometry: {
-      type: 'Point',
-      coordinates: [place.location.lng, place.location.lat],
-    },
-    properties: {
-      city: place.city.slug.current,
-      name: place.name,
-      slug: place.slug,
-      note: place.note,
-      url: place.url,
-      address: place.address,
-    },
-  }));
+  const features = result.data.allPlace.map(
+    (place: SanityPlaceResponse, index: number) => ({
+      type: 'Feature',
+      id: index + 1, // Mapbox requires a numerical ID
+      geometry: {
+        type: 'Point',
+        coordinates: [place.location.lng, place.location.lat],
+      },
+      properties: {
+        city: place.city.slug.current,
+        name: place.name,
+        slug: place.slug.current,
+        note: place.note,
+        url: place.url,
+        address: place.address,
+      },
+    }),
+  );
 
   const geoJSON = {
     type: 'geojson',
